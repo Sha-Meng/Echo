68e04af1a407460d225c275cf75ce995c9dbfae3
Sha-Meng <54922354+Sha-Meng@users.noreply.github.com>
2020-12-06 20:56:53 +0800

    # create Echo project by shameng

---
+++ Echo/Assets/Scripts/Echo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour
+{
+    // 定义套接字
+    Socket socket;
+    public InputField input;
+    public Text text;
+
+    // 定义连接按钮
+    public void Connection()
+    {
+        // 判断是否连接
+        if (socket == null || !socket.Connected)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // connect
+            socket.Connect("127.0.0.1", 8888);
+        }
+    }
+
+    public void Send()
+    {
+        // 判断是否连接
+        if (socket == null || !socket.Connected)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // connect
+            socket.Connect("127.0.0.1", 8888);
+        }
+        // 获取输入框文字
+        string sendStr = input.text;
+        byte[] sendByte = System.Text.Encoding.Default.GetBytes(sendStr);
+        socket.Send(sendByte);
+
+        // 获取服务器返回的数据
+        byte[] readBuff = new byte[1024];
+        int count = socket.Receive(readBuff);
+        string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+        text.text = recvStr;
+
+        // 关闭连接
+        socket.Close();
+    }
+}
---
+++ EchoServer/EchoServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Socket 
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);
+
+            listenfd.Bind(ipEp);
+
+            // Listen
+            listenfd.Listen(0);
+            Console.WriteLine("[Server] 启动成功");
+
+            while (true)
+            {
+                // Accept
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("[Server] 连接建立");
+
+                // Read
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+                Console.WriteLine("[Server] 接收：" + readStr);
+
+                // Send
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+        }
+    }
+}
